79e5b42be381b4b92423691fbe81e01df7e2335a
Zadock <malobazadock@gmail.com>
2021-03-29 23:32:29 +0300

    Basic setup for rendering a triangle

---
+++ build/CMakeFiles/glut_test1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/glut_test1.dir/main.cpp.o"
+  "glut_test1"
+  "glut_test1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/glut_test1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <GLES3/gl3.h>
+#include <GL/glut.h>
 
-int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
-    return 0;
+void render(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glBegin(GL_TRIANGLES);
+        glVertex3f(-0.5f, -0.5f, 0.0f);
+        glVertex3f(0.5f, 0.0f, 0.0f);
+        glVertex3f(0.0f, 0.0f, 0.0f);
+    glEnd();
+
+    glutSwapBuffers();
+}
+
+int main(int argc, char **argv) 
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    glutInitWindowPosition(100,100);
+    glutCreateWindow("My GL APP");
+    glutDisplayFunc(render);
+
+    glutMainLoop();
+    return EXIT_SUCCESS;
 }
